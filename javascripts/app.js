0b90177cd20a7dd92af9aeb67b25a04519f249ee
banatato <banatato@users.noreply.github.com>
2015-03-02 18:48:27 -0800

    build content for first and third tab

--- javascripts/app.js
+++ javascripts/app.js
@@ -11,27 +11,49 @@ var main = function () {
 	];
 
 	$(".tabs span").toArray().forEach(function (element) {
+		var $element = $(element);
+
 		//create ae click handler for this element
-		$(element).on("click", function () {
-			var $element = $(element),
-				$content;
+		$element.on("click", function () {
+			var $content,
+				$input,
+				$button,
+				i;
 
 			$(".tabs span").removeClass("active");
 			$element.addClass("active");
 			$("main .content").empty();
 
 			if ($element.parent().is(":nth-child(1)")) {
-				console.log("FIRST TAB CLICKED!");
+				// newest first, so we have to go through
+				// the array backwards
+				$content = $("<ul>");
+				for (i = toDos.length-1; i>= 0; i--) {
+					$content.append($("<li>").text(toDos[i]));
+				}
 			} else if ($element.parent().is(":nth-child(2)")) {
+				//oldest first, so we go through the array forwards
 				$content = $("<ul>");
 				toDos.forEach(function (todo) {
 					$content.append($("<li>").text(todo));
 				});
-				$("main .content").append($content);
 			} else if ($element.parent().is(":nth-child(3)")) {
-				console.log("THIRD TAB CLICKED!");
+				//input a new to-do
+				$input = $("<input>"),
+				$button = $("<button>").text("+");
+
+				$button.on("click", function() {
+					if ($input.val() !== "") {
+						toDos.push($input.val());
+						$input.val("");
+					}
+				});
+
+				$content = $("<div>").append($input).append($button);
 			}
 
+			$("main .content").append($content);
+			
 			return false;
 		});
 	});
