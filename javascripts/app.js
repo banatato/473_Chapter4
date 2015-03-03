3317dd41c6aff3b14120343572f0d27756478cfe
banatato <banatato@users.noreply.github.com>
2015-03-02 18:25:21 -0800

    Use of forEach loop and click handler

--- javascripts/app.js
+++ javascripts/app.js
@@ -1,16 +1,15 @@
 var main = function () {
 	"use strict";
 
-	var tabNumber;
-
-	for (tabNumber = 1; tabNumber <=3; tabNumber++) {
-		var tabSelector = ".tabs a:nth-child(" + tabNumber + ") span";
-		$(tabSelector).on("click", function (event) {
+	$(".tabs span").toArray().forEach(function (element) {
+		//create ae click handler for this element
+		$(element).on("click", function () {
 			$(".tabs span").removeClass("active");
-			$(event.target).addClass("active");
+			$(element).addClass("active");
+			$("main .content").empty();
 			return false;
 		});
-	}
+	});
 };
 
 $(document).ready(main);
