d1361df393bfd3475a9f496623a8a381c1c50069
banatato <banatato@users.noreply.github.com>
2015-03-02 18:34:32 -0800

    store to-dos as strings in an array

--- javascripts/app.js
+++ javascripts/app.js
@@ -1,12 +1,32 @@
 var main = function () {
 	"use strict";
 
+	var toDos = [
+		"Finish writing this book",
+		"Take Gracie to the park", 
+		"Answer emails",
+		"Prep for Monday's class",
+		"Make up some new ToDos",
+		"Get Groceries"
+	];
+
 	$(".tabs span").toArray().forEach(function (element) {
 		//create ae click handler for this element
 		$(element).on("click", function () {
+			var $element = $(element);
+
 			$(".tabs span").removeClass("active");
-			$(element).addClass("active");
+			$element.addClass("active");
 			$("main .content").empty();
+
+			if ($element.parent().is(":nth-child(1)")) {
+				console.log("FIRST TAB CLICKED!");
+			} else if ($element.parent().is(":nth-child(2)")) {
+				console.log("SECOND TAB CLICKED!");
+			} else if ($element.parent().is(":nth-child(3)")) {
+				console.log("THIRD TAB CLICKED!");
+			}
+
 			return false;
 		});
 	});
